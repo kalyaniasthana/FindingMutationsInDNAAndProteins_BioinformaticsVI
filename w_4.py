26bd31bfcda6db2ca5034095bfbef3a6099b9fee
Kalyani Asthana <kalyaniasthana@gmail.com>
2019-06-21 00:06:58 +0530

    probability of an outcome given hidden path

--- w_4.py
+++ w_4.py
@@ -12,7 +12,7 @@ def input_transition(file, x):
 
 		for line in f:
 			l = line.strip()
-			l = line.split('\t')
+			l = l.split('\t')
 			if l[0] == 'A':
 				transition_matrix[l[0]]['A'] = float(l[1])
 				transition_matrix[l[0]]['B'] = float(l[2])
@@ -29,9 +29,42 @@ def probability_of_hidden_path(path, transition_matrix):
 		pr *= transition_matrix[path[i]][path[i+1]]
 	return pr
 
+def input_emission(file, states, alphabet_x):
+	emission_matrix = {}
+	for i in states:
+		emission_matrix[i] = {}
+	for i in emission_matrix:
+		for j in alphabet_x:
+			emission_matrix[i][j] = 0
 
+	with open(file) as f:
+		for line in f:
+			l = line.strip()
+			l = l.split('\t')
+			for j in range(len(alphabet_x)):
+				emission_matrix[l[0]][alphabet_x[j]] = float(l[j+1])
+
+	return emission_matrix
+
+def probability_of_outcome_given_hidden_path(sequence, hidden_path, emission_matrix):
+	pr = 1
+	hidden_path = list(hidden_path)
+	sequence = list(sequence)
+	for i in range(len(hidden_path)):
+		pr *= emission_matrix[hidden_path[i]][sequence[i]]
+	return pr
+
+'''
 file = 'hmm.txt'
 path = 'BABAAABABBBBABAABBABAAABABBABAAAABAABABBBAAABAAAAB'
 states = 'A B'.split(' ')
 transition_matrix = input_transition(file, states)
 print(probability_of_hidden_path(path, transition_matrix))
+'''
+file = 'hmm.txt'
+sequence = 'zzzyzzxzzyyzxyxxxyyzxyyxxxyzzzxyzzzzyzzzxxxzyyzxyx'
+hidden_path = 'BAABAAAAABAAAAABAAAABABBABAAAABAABABAABBAAABBABBAB'
+states = 'A B'.split(' ')
+alphabet_x = 'x y z'.split(' ')
+emission_matrix = input_emission(file, states, alphabet_x)
+print(probability_of_outcome_given_hidden_path(sequence, hidden_path, emission_matrix))
